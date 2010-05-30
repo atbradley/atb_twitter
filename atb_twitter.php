b8f88d088631ae50f05320df2ab0645007cb8d69
Adam Bradley <hisself@adambradley.net>
2010-05-29 21:05:12 -0400

    Version 0.9.4. Add support for new (versionable) Twitter API path. Make plugin timezone-aware. Add classes to links created within tweet text. Enable wraptags for links.

--- atb_twitter.php
+++ atb_twitter.php
@@ -14,7 +14,7 @@ $plugin['name'] = 'atb_twitter';
 // 1 = Plugin help is in raw HTML.  Not recommended.
 # $plugin['allow_html_help'] = 0;
 
-$plugin['version'] = '0.9.3.2';
+$plugin['version'] = '0.9.4';
 $plugin['author'] = 'Adam T. Bradley';
 $plugin['author_uri'] = 'http://twitter.com/daydreamlab';
 $plugin['description'] = 'Display Twitter/StatusNet feeds on your website.';
@@ -82,7 +82,14 @@ h4. Tags for twitter forms:
 
 Only a few of these tags take any attributes:
 
-* *atb_tw_text* returns the tweet's text.  It takes one optional attribute, @linkify@. With @linkify@ set to 1 (the default), URLs, at-replies and hashtags are converted to links; with @linkify@ set to 0, the text will be left alone.
+* *atb_tw_text* returns the tweet's text. If the @linkify@ attribute is left at 1 (its default), URLs, hashtags, and @replies will be converted to links and given CSS classes: all links are given the class "atb_status_link"; converted URLs additionally take the class "atb_status_weblink"; @replies get "atb_status_atreply" as well as a customized class based on the username mentioned, like "atb_status_atreply_to_[username]"; hashtags get "atb_status_hashtag" and another class based on the hashtag itself, like "atb_hashtag_[tag]". This tag takes the following attributes, all optional:
+** *linkify:* With @linkify@ set to 1 (the default), URLs, at-replies and hashtags are converted to links; with @linkify@ set to 0, the text will be left alone. The remaining attributes will be ignored if @linkify@ is set to 0.
+** *link_wrap:* The name of a tag to wrap around URLS that have been converted to links.
+** *link_wrapclass:* The class of the URL wraptags.
+** *atreply_wrap:* The name of a tag to wrap around @reply links.
+** *atreply_wrapclass:* The class for @reply wraptags.
+** *hashtag_wrap:* The name of a tag to wrap around hashtag links.
+** *hashtag_wrapclass:* The class for hashtag wraptags.
 * *atb_tw_created_at* returns the date of the tweet. It takes two attributes: @format@, which may be any value accepted by "txp:posted":http://textpattern.net/wiki/index.php?title=posted and defaults to the "Archive date format" as set in Textpattern's preferences; and @tz@, which determines if the time used is the local time (as determined by Textpattern's timezone--this is the default setting), GMT (set @tz@ to "1"), or the time zone Twitter reports for the user (set @tz@ to "2").
 * *atb_tw_user_created_at* returns the date the Twitter account being read was created. It takes the same attributes as @atb_tw_created_at@.
 
@@ -123,7 +130,7 @@ This is just a basic form that displays my most recent 20 statuses:
 bc.. <txp:atb_twitter count="20" user="daydreamlab">
 
 <txp:atb_tw_if_first_tweet>
-<h2><a href="http://twitter.com/<txp:atb_tw_user_screen_name />"><txp:atb_tw_user_name /> on Twitter</a></h2>
+<h2><a href="http://twitter.com/<txp:atb_tw_user_screen_name />">@daydreamlab on Twitter</a></h2>
 <ul>
 </txp:atb_tw_if_first_tweet>
 
@@ -137,7 +144,7 @@ bc.. <txp:atb_twitter count="20" user="daydreamlab">
 in reply to <txp:atb_tw_in_reply_to_screen_name />
 </a>
 </txp:atb_tw_if_is_reply>
-
+</li>
 
 <txp:atb_tw_if_last_tweet>
 </ul>
@@ -145,11 +152,7 @@ in reply to <txp:atb_tw_in_reply_to_screen_name />
 
 </txp:atb_twitter>
 
-
-h3. To_Do
-
-* More/better error handling.
-* Better/clearer documentation.
+h3. To Do
 
 # --- END PLUGIN HELP ---
 <?php
@@ -174,8 +177,8 @@ function atb_twitter($atts, $thing) {
                         'count' => 5,
                         'cache' => 15,
                         'form' => '',
-                        'api_base' => 'twitter.com',
-                        'site_base' => '',
+                        'api_base' => '',
+                        'site_base' => 'twitter.com',
                         'update_lastmod' => 1
                        ),
                   $atts));
@@ -193,9 +196,18 @@ function atb_twitter($atts, $thing) {
         return '';
     }
     
-    $api_base = trim($api_base, '/ ');
     if ( !$site_base ) {
-        $site_base = preg_replace('/(\/api$)/', '', $api_base);
+        if ( $api_base === '' ) {
+            $api_base = 'api.twitter.com/1';
+            $site_base = 'twitter.com';
+        } else {
+            $api_base = trim($api_base, '/ ');
+            $site_base .= preg_replace('/(\/api$)/', '', $api_base);
+        }
+    } elseif ( 'twitter.com' === $site_base ) {
+        $api_base = 'api.twitter.com/1';
+    } elseif ( !$api_base ) {
+        $api_base = $site_base.'/api';
     }
     
     $tweets = atb_get_tweets($user, $count, $api_base);
@@ -239,12 +251,22 @@ function atb_twitter($atts, $thing) {
 /* Txp form tags
   ----------------------------------------------------------------------------*/
 function atb_tw_text($atts) {
-    global $atb_thistweet; 
-    extract(lAtts(array('linkify' => 1),
-              $atts));
+    global $atb_thistweet;
+    
+    $atts = lAtts(array('linkify' => 1,
+                        'link_wrap' => '',
+                        'link_wrapclass' => '',
+                        'atreply_wrap' => '',
+                        'atreply_wrapclass' => '',
+                        'hashtag_wrap' => '',
+                        'hashtag_wrapclass' => ''),
+              $atts);
+    
+    extract($atts);
     
     if ( $linkify )  { return atb_linkify_tweet($atb_thistweet->text,
-                                                $atb_thistweet['site_base']); }
+                                                (string)$atb_thistweet['site_base'],
+                                                $atts); }
     else { return $atb_thistweet->text; }
 }
 
@@ -413,7 +435,7 @@ function atb_get_tweet_cache($hash, $maxage = 1800)
     global $prefs;
     $cachefile = $prefs['tempdir'].'/atb_twitter_'.$hash.'.txt';
     
-    $mtime = filemtime($cachefile);
+    $mtime = file_exists($cachefile) ? filemtime($cachefile) : 0;
 
     if ( $mtime &&
             ( (time() - $mtime) < $maxage || $maxage == 0 ) ) {
@@ -444,7 +466,6 @@ function atb_put_tweet_cache($hash, $data)
 function atb_get_tweets($user, $count, $api_base = 'twitter.com')
 {
     $url = "http://$api_base/statuses/user_timeline/$user.xml?count=$count";
-    
     $tweets = @simplexml_load_file($url);
     
     return $tweets ? $tweets : false;
@@ -452,30 +473,66 @@ function atb_get_tweets($user, $count, $api_base = 'twitter.com')
 
 /* Utility function
   ----------------------------------------------------------------------------*/
-function atb_linkify_tweet($tweet, $site_base = 'twitter.com')
+
+function atb_linkify_tweet($tweet, $site_base = 'twitter.com', array $atts = null)
 {
     //StatusNet handles hashtags differently, so we need to know whether
     //this is Twitter or SN.
-    $using_twitter = ($api_base === 'twitter.com');
+    $using_twitter = ($site_base === 'twitter.com');
+    extract($atts);
     
     $re = array();
+    
     //Convert http: addresses to links:
-    $re['/(https?:\/\/[^ ]+)/'] = '<a href="$1">$1</a>';
+    $re['/(https?:\/\/[^ ]+)/'] = create_function("\$matches, \$wrap = '$link_wrap', \$wrapclass = '$link_wrapclass'",
+                                                  '
+                                                   $wrapclass = $wrapclass ? " class=\"$wrapclass\"" : "";
+                                                   $outp = "<a class=\"atb_status_weblink atb_status_link\" href=\"{$matches[0]}\">{$matches[0]}</a>";
+                                                   if ( $wrap ) { $outp = tag($outp, $wrap, $wrapclass); }
+                                                   return $outp;
+                                                  ');
     
     //Convert @replies to links:
-    $re['/(@([0-9a-zA-Z-_]+))/'] = "<a href=\"http://$site_base/$2\">$1</a>";
+    $re['/(@([0-9a-zA-Z-_]+))/'] = create_function("\$matches, \$site_base = '$site_base', \$wrap = '$atreply_wrap', \$wrapclass = '$atreply_wrapclass'",
+                                              '
+                                               $class2 = "atb_atreply_to_".$matches[2];
+                                               $wrapclass .= $wrapclass ? " ": "";
+                                               $wrapclass = " class=\"$wrapclass$class2\"";
+                                               $outp = "<a class=\"atb_status_atreply atb_status_link atb_atreply_to_{$matches[2]}\" href=\"http://$site_base/{$matches[2]}\">{$matches[0]}</a>";
+                                               if ( $wrap ) { $outp = tag($outp, $wrap, $wrapclass); }
+                                               return $outp;
+                                              ');
     
-    //Convert hashtags to links:
     $re['/(#([a-zA-Z0-9-_]+))/'] = $using_twitter ?
-        '<a href="http://search.twitter.com/search?result_type=recent&amp;q=%23$2">$1</a>' :
-        "#<a href=\"http://$site_base/tag/$2\">$2</a>";
+                                   create_function("\$matches, \$site_base = '$site_base', \$wrap = '$hashtag_wrap', \$wrapclass = '$hashtag_wrapclass'",
+                                              '
+                                               $class2 = "atb_hashtag_".$matches[2];
+                                               $wrapclass .= $wrapclass ? " ": "";
+                                               $wrapclass = " class=\"$wrapclass$class2\"";
+                                               $outp = "<a class=\"atb_status_hashtag atb_status_link atb_hashtag_{$matches[2]}\" href=\"http://search.twitter.com/search?result_type=recent&amp;q=%23{$matches[2]}\">{$matches[0]}</a>";
+                                               if ( $wrap ) { $outp = tag($outp, $wrap, $wrapclass); }
+                                               return $outp;
+                                              ') :
+                                   create_function("\$matches, \$site_base = '$site_base', \$wrap = '$hashtag_wrap', \$wrapclass = '$hashtag_wrapclass'",
+                                              '
+                                               $class2 = "atb_hashtag_".$matches[2];
+                                               $wrapclass .= $wrapclass ? " ": "";
+                                               $wrapclass = " class=\"$wrapclass$class2\"";
+                                               $outp = "#<a class=\"atb_status_hashtag atb_status_link atb_hashtag_{$matches[2]}\" href=\"http://$site_base/tag/{$matches[2]}\">{$matches[2]}</a>";
+                                               if ( $wrap ) { $outp = tag($outp, $wrap, $wrapclass); }
+                                               return $outp;
+                                              ');
     
     foreach ($re as $regex => $replace) {
-        $tweet = preg_replace($regex, $replace, $tweet);
+        $tweet = preg_replace_callback($regex, $replace, $tweet);
     }
     
     return $tweet;
 }
+
+/* preg_replace_callback callbacks
+  ----------------------------------------------------------------------------*/
+  
 
 # --- END PLUGIN CODE ---
 
